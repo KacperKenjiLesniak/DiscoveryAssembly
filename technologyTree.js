7a6b913cd17a24b52414c7f51a97aa4ab2b04344
= <kenjikacper@gmail.com>
2017-11-18 12:17:19 +0100

    Technology tree working good.

--- technologyTree.js
+++ technologyTree.js
@@ -14,14 +14,14 @@ function TechnologyTree(){
     this.createMainNode = function () {
         node = new TechnologyNode();
         node.createTechnologyNode(
-            [Math.abs(Math.random()*10), Math.abs(Math.random()*10), Math.abs(Math.random()*10)]);
+            [Math.round(Math.random()*10), Math.round(Math.random()*10), Math.round(Math.random()*10)]);
         this.existingNodes.push(node);
     };
     this.addNode = function(){
         node = new TechnologyNode();
         node.createTechnologyNode(
-            [Math.abs(Math.random()*10), Math.abs(Math.random()*10), Math.abs(Math.random()*10)]);
-        productNodeIndex = Math.abs(Math.random()*this.existingNodes.length);
+            [Math.round(Math.random()*10), Math.round(Math.random()*10), Math.round(Math.random()*10)]);
+        productNodeIndex = Math.floor(Math.random()*this.existingNodes.length);
         node.products = [this.existingNodes[productNodeIndex]];
         this.existingNodes[productNodeIndex].ingredients.push(node);
         this.existingNodes.push(node);
@@ -35,11 +35,22 @@ tree.addNode();
 tree.addNode();
 tree.addNode();
 for (var i =0; i<tree.existingNodes.length; ++i){
+    document.write("Node<br>");
     document.write(tree.existingNodes[i].knowledgeFields);
     document.write("<br>");
-    document.write(tree.existingNodes[i].products);
+    document.write("Products: <br>");
+    for (var k =0; k<tree.existingNodes[i].products.length; k++) {
+        document.write(tree.existingNodes[i].products[k].knowledgeFields);
+        document.write("<br>");
+    }
     document.write("<br>");
-    document.write(tree.existingNodes[i].ingredients);
+    document.write("Ingredients:<br>");
+    for (var k =0; k<tree.existingNodes[i].ingredients.length; k++) {
+        document.write(tree.existingNodes[i].ingredients[k].knowledgeFields);
+        document.write("<br>");
+    }
+    document.write("<br>");
+    document.write("<br>");
     document.write("<br>");
     document.write("<br>");
 }
