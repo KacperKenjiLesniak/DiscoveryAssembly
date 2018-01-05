f05c9f49c31d7bdc4b809f062c8f6165ece7c617
sebastians96 <skoczen.kontakt@gmail.com>
2018-01-05 13:34:41 +0100

    Working read write to file, problems with class functions...

--- src/mainLoop.js
+++ src/mainLoop.js
@@ -38,34 +38,3 @@ function day(){
     updateTeams(teams);
     setTimeout(day, DAYLENGTH);
 }
-
-function download(data,filename) {
-    var file = new Blob([data], {type: "text/plain;charset=utf-8"});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-            url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        }, 0);
-    }
-}
-
-function readSingleFile(e) {
-    var file = e.target.files[0];
-    if (!file) {
-        return;
-    }
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        return e.target.result;
-        //displayContents(contents);
-    };
-    reader.readAsText(file);
-}
---
+++ src/readWriteFiles.js
@@ -0,0 +1,43 @@
+var bufferTree = "";
+var bufferTeams = "";
+function download(data,filename) {
+    var file = new Blob([data], {type: "text/plain;charset=utf-8"});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+
+function readTreeFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        bufferTree = e.target.result;
+    };
+    reader.readAsText(file);
+}
+
+function readTeamsFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        bufferTeams = e.target.result;
+    };
+    reader.readAsText(file);
+}
